533dbd85600e8431973c65a6425ed8dadb14472e
Aynuo <10zerobot@gmail.com>
2024-03-09 22:12:12 +0330

    Create show_message.sh

---
+++ variables/show_message.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+shutdown_time="07:10 PM"
+start_time="08:00 AM"
+
+echo "hi your system will be start on ${start_time}"
+echo "you can work till shutdown time"
+echo "shutdown time is ${shutdown_time}"
+
+echo "good luck"
+exit 0
