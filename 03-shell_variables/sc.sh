4ba648e353f116b59b7e9867467b8c273ba85c05
Aynuo <10zerobot@gmail.com>
2024-03-10 22:10:15 +0330

    Create sc.sh

---
+++ 03-shell_variables/sc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# PATH: list of folders that the shell will search for executable files
+echo "------------------------------------------------------"
+echo $PATH
+echo "------------------------------------------------------"
+
+# HOME: Used to store the absolute path to the current user's home directory.
+
+echo "------------------------------------------------------"
+echo $HOME
+echo "------------------------------------------------------"
+
+# USER: Contains the username of the current user.
+echo "------------------------------------------------------"
+echo $USER
+echo "------------------------------------------------------"
+
+# HOSTNAME: the name of the current computer
+
+exit 0
