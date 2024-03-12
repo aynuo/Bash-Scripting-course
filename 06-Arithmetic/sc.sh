224cfff2face2d4209f24caf6bf5ac94b2f300f7
Aynuo <10zerobot@gmail.com>
2024-03-12 21:54:36 +0330

    Create sc.sh

---
+++ 06-Arithmetic/sc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo $(( 4 + 2))
+
+x=8
+y=3
+echo $(($x + $y))
+echo $(( x + y))
+
+echo $(( x + y))
+echo $(( x - y))
+echo $(( x / y))
+echo $(( x * y))
+
+echo $(( (2 + 3) * 10))
+echo $(( 2 + 3 * 10))
