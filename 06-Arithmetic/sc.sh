5a21cc553186fa5ee03f566bfea68116d041ef63
Aynuo <10zerobot@gmail.com>
2024-03-12 22:05:32 +0330

    Update sc.sh

--- 06-Arithmetic/sc.sh
+++ 06-Arithmetic/sc.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
+echo "Basic: "
 echo $(( 4 + 2))
 
+echo "Use Variables:"
 x=8
 y=3
+echo "use $:"
 echo $(($x + $y))
+
+echo "without $:"
 echo $(( x + y))
 
+echo "operators:"
+echo ""
+echo "+:"
 echo $(( x + y))
+
+echo "-:"
 echo $(( x - y))
+
+echo "/:"
 echo $(( x / y))
+
+echo "*:"
 echo $(( x * y))
 
+echo "priority"
+echo ""
+echo " (2+3) *10: "
 echo $(( (2 + 3) * 10 ))
+
+echo " 2+3*10: "
 echo $(( 2 + 3 * 10 ))
 
+echo "power:"
 echo $(( 4 ** 2 ))
 
+echo "modular:"
 echo $(( 4 % 2 ))
